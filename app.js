093bdbc9133122ea2e1a59199bd97277e53d5c69
JK_Potato <100048313+JKPotato-Computer@users.noreply.github.com>
2024-05-24 20:56:21 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,15 +2,13 @@ let express = require("express");
 let app = express();
 const port = process.env.PORT || 5000;
 
-let serv = require("http").createServer(app);
+let serv = require("http").Server(app);
 
 app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/client/index.html");
 })
 
 app.use("/client",express.static(__dirname + "/client"));
-
-app.listen((port == 1000) ? (port + 1000) : (port - 1000));
 serv.listen(port);
 
 let SOCKET_LIST = {};
@@ -19,8 +17,9 @@ let io = require("socket.io")(serv, {
 	cors: {
 		origin : "http://localhost:5000",
 	},
-	resource: '/ws/socket.io'
 });
+
+
 io.sockets.on("connection", function(socket) {
 	socket.id = Math.random();
 	SOCKET_LIST[socket.id] = socket;

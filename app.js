5818ad8e9ebc7101bea3ff2d13f4f16e412c0f06
JKPotato-Computer <jkpotato.compurer@gmail.com>
2024-06-04 10:55:36 -0500

    last commit

--- app.js
+++ app.js
@@ -28,62 +28,124 @@ app.get("/", function(req, res) {
 	res.sendFile(__dirname + "/client/index.html");
 })
 
-function countForSockets(serverID) {
-	let i = 0;
+function createPublicServerInfo(serverID) {
+	let server = AVAILABLE_SERVERS[serverID]
+	let publicServerInfo = {
+		leader: server.leader,
+		playerList : server.playerList
+	}
+
+	return publicServerInfo
+}
+
+function countForSockets(serverID,returnList) {
 	let socketList = [];
 	for (const socketID in SOCKET_LIST) {
 		let sockets = SOCKET_LIST[socketID];
 		if (sockets.rooms.has(serverID)) {
-			i++;
-			socketList.push(sockets.id);
+			socketList.push(sockets);
 		}
 	}
-	return i;
+	return (returnList) ? socketList : socketList.length;
+}
+
+function updatePlayerInfo(socketID) {
+	for (const serverID in AVAILABLE_SERVERS) {
+		const serverData = AVAILABLE_SERVERS[serverID];
+		if (serverData.playerList[socketID]) {
+			serverData.playerList[socketID] = SOCKET_DATA[socketID];
+		}
+	}
 }
 
 function serverStatsChange(serverID) {
-	let data = {
-		sockets: countForSockets(serverID)
-	};
-
 	for (const sID in SOCKET_LIST) {
 		let iSocket = SOCKET_LIST[sID];
 		if (iSocket.rooms.has(serverID)) {
-			iSocket.emit("serverStatsChange",data);
+			iSocket.emit("serverStatsChange",createPublicServerInfo(serverID));
 		}
 	}
 }
+
+function updateServerList(socket) {
+	let publicServers = []
+	for (const serverID in AVAILABLE_SERVERS) {
+		let server = AVAILABLE_SERVERS[serverID]	
+
+		if (server.publicServer) {
+			publicServers.push(serverID);
+		}
+	}
+
+	if (socket) {
+		socket.emit("updateServerList",publicServers)
+	} else {
+		io.emit("updateServerList",publicServers)
+	}
+}
+
+function leaveRoom(serverID,socket) {
+
+	delete AVAILABLE_SERVERS[serverID].playerList[socket.id]
+	socket.leave(serverID);
+
+	if (socket.id == AVAILABLE_SERVERS[serverID].leader) {
+		// Choose random leader
+		const currentPlayers = Object.keys(AVAILABLE_SERVERS[serverID].playerList);
+		const randomNumber = currentPlayers[Math.floor(Math.random() * currentPlayers.length)];
+		AVAILABLE_SERVERS[serverID].leader = randomNumber;
+	}
+
+	serverStatsChange(serverID);
+	socket.emit("serverLeave");
+}
+
 
 io.sockets.on("connection", function(socket) {
 
 	socket.id = Math.random();
 	SOCKET_LIST[socket.id] = socket;
 	SOCKET_DATA[socket.id] = {
-		 name : ""
+		 name : "",
+		 id: socket.id
 	};
+
+	updateServerList(socket);
 
 	// User Data
 
-	socket.on("updateData",function(data) {
+	socket.emit("setClientSocketData",SOCKET_DATA[socket.id])
+
+	socket.on("updateData",function(data,serverId) {
 		SOCKET_DATA[socket.id] = data;
-	})
+		updatePlayerInfo(socket.id);
+
+		if (serverId) {
+			serverStatsChange(serverId);
+		}
+	});
 
 	// Servers
 
-	socket.on("createServer",function() {
+	socket.on("createServer",function(visibility) {
+		if (socket.rooms.size > 0) {
+			return;
+		}	
+
 		const zeroPad = (num, places) => String(num).padStart(places, '0')
 
 		let IDList = [];
 		let serverID = "G-";
 		let serverData = {
 			leader: socket.id, // Creator's 'Socket ID
+			publicServer : (visibility == "public") ? true : false,
 			playerList : {
 				[socket.id] : SOCKET_DATA[socket.id]
 			}
 		}
 
 		for (let i =0;i < 3;i++) {
-			IDList[i] = zeroPad(Math.floor(Math.random() * 100),3);
+			IDList[i] = zeroPad(Math.floor(Math.random() * 1000),3);
 		}
 
 		serverID = "G-" + IDList.join("");
@@ -92,7 +154,11 @@ io.sockets.on("connection", function(socket) {
 		socket.join(serverID);
 		socket.emit("serverJoin",serverID);
 		serverStatsChange(serverID);
-	})
+		
+		if (visibility == "public") {
+			updateServerList();
+		}
+	});
 
 	socket.on("forceClear",function(serverID) {
 		for (const sID in SOCKET_LIST) {
@@ -103,38 +169,60 @@ io.sockets.on("connection", function(socket) {
 			}
 		}
 		countForSockets(serverID);
-	})
+	});
 
 	socket.on("serverJoin",function(serverID) {
 		if (!AVAILABLE_SERVERS[serverID]) {
 			return;
 		}
+
+		for (const serverID in AVAILABLE_SERVERS) {
+			if (socket.id in AVAILABLE_SERVERS[serverID].playerList) {
+				return;
+			}
+		}
 		
-		AVAILABLE_SERVERS[serverID].playerList[socket.id] = SOCKET_DATA[socket.id]
+		AVAILABLE_SERVERS[serverID].playerList[socket.id] = SOCKET_DATA[socket.id];
 
 		socket.join(serverID);
 		socket.emit("serverJoin",serverID);
 		serverStatsChange(serverID);
-	})
+	});
 
 	socket.on("serverLeave",function(serverID) {
 		if (!AVAILABLE_SERVERS[serverID]) {
 			return;
 		}
-		
-		delete AVAILABLE_SERVERS[serverID].playerList[socket.id]
 
-		socket.leave(serverID);
-		socket.emit("serverLeave");
+		leaveRoom(serverID,socket);
+	});
+
+	socket.on("kickPlayer",function(playerID,serverID) {
+		let server = AVAILABLE_SERVERS[serverID];
+		if (server.leader != socket.id) {
+			return;
+		}
+
+		leaveRoom(serverID,SOCKET_LIST[playerID]);
+	});
+	
+	socket.on("transferOwnership",function(playerID,serverID) {
+		let server = AVAILABLE_SERVERS[serverID];
+		
+		if (server.leader != socket.id) {
+			return;
+		}
+
+		server.leader = playerID;
 		serverStatsChange(serverID);
-	})
+	});
 	
 	socket.on("disconnect", function() {
 
 		// Leave All Servers
 		let rooms = socket.rooms.keys();
 		for (const room of rooms) {
-			socket.leave(room);
+			leaveRoom(room,socket);
 		}
 
 		delete SOCKET_LIST[socket.id];
@@ -142,10 +230,16 @@ io.sockets.on("connection", function(socket) {
 });
 
 setInterval(function() {
-	for (const serverID in AVAILABLE_SERVERS) {
+	for (const serverID in AVAILABLE_SERVERS) {	
+
 		if (countForSockets(serverID) == 0) {
+			let publicServer = AVAILABLE_SERVERS[serverID].publicServer;
 			console.log("Server closed/at 0: G-" + serverID);
 			delete AVAILABLE_SERVERS[serverID];
-		}	
+
+			if (publicServer) {
+				updateServerList();
+			}
+		}
 	}
 },(1000/25))
--- client/main.js
+++ client/main.js
@@ -1,11 +1,69 @@
 // prevent using up my precious Render server :(
 const isSafe = ['localhost', '127.0.0.1', '::1'].includes(location.hostname) || /^\d+(\.\d+){3}$/.test(location.hostname) || req.url.startsWith('file://');
 let socket = io((isSafe) ? ("") : ("https://budget-uno.onrender.com"));
+
 let serverID = "";		
+let socketData;
+let serverIDList = [];
+
+socket.on("updateServerList", function(list) {
+	serverIDList = list;
+
+	while (document.querySelector("#serverList").firstChild) {
+		document.querySelector("#serverList").removeChild(document.querySelector("#serverList").lastChild);
+	}
+
+	for (const id of serverIDList) {
+		const serverButton = document.createElement("button");
+		serverButton.textContent = id;
+		document.querySelector("#serverList").appendChild(serverButton);
+
+		serverButton.onclick = function() {
+			socket.emit("serverJoin",id);
+		}
+	}
+});
 
 socket.on("serverStatsChange", function(data) {
-	console.log(data);
-	document.querySelector("#serverSockets").textContent = "Sockets: " + data.sockets;
+	document.querySelector("#serverSockets").textContent = "Sockets: " + Object.keys(data.playerList).length;
+	while (document.querySelector("#playerList").firstChild) {
+		document.querySelector("#playerList").removeChild(document.querySelector("#playerList").lastChild)
+	}
+
+	for (const id in data.playerList) {
+		const playerData = data.playerList[id];
+		const playerDiv = document.createElement("div");
+
+		const playerLabel = document.createElement("span");
+		playerLabel.textContent = ((data.leader == id) ? ("LEADER: ") : ("")) + playerData.name;
+
+		const kickButton = document.createElement("button")
+		kickButton.textContent = "Kick";
+
+		const transferButton = document.createElement("button");
+		transferButton.textContent = "Transfer";
+
+		kickButton.onclick = function() {
+			socket.emit("kickPlayer",id,serverID);
+		}
+
+		transferButton.onclick = function() {
+			socket.emit("transferOwnership",id,serverID);
+		}
+
+		playerDiv.appendChild(playerLabel);
+
+		if ((data.leader != id) && (data.leader == socketData.id)) {
+			playerDiv.appendChild(kickButton);
+			playerDiv.appendChild(transferButton);
+		}
+
+		document.querySelector("#playerList").appendChild(playerDiv);
+	}
+});
+
+socket.on("setClientSocketData", function(data) {
+	socketData = data;
 });
 
 socket.on("serverLeave", function(id) {
@@ -16,6 +74,10 @@ socket.on("serverLeave", function(id) {
 	document.querySelector("#leaveServer").className = "invisible";
 	document.querySelector("#destroyServer").className = "invisible";
 	serverID = "";
+
+	while (document.querySelector("#playerList").firstChild) {
+		document.querySelector("#playerList").removeChild(document.querySelector("#playerList").lastChild)
+	}
 })
 
 socket.on("serverJoin", function(id) {
@@ -26,8 +88,13 @@ socket.on("serverJoin", function(id) {
 	serverID = id;
 })
 
-document.querySelector("#createServer").onclick = function() {
-	socket.emit("createServer");
+document.querySelector("#createPubServer").onclick = function() {
+	socket.emit("createServer","public");
+	document.querySelector("#destroyServer").className = "";
+}
+
+document.querySelector("#createPrivServer").onclick = function() {
+	socket.emit("createServer","private");
 	document.querySelector("#destroyServer").className = "";
 }
 
@@ -41,4 +108,16 @@ document.querySelector("#leaveServer").onclick = function() {
 
 document.querySelector("#destroyServer").onclick = function() {
 	socket.emit("forceClear",serverID);
+}
+
+document.querySelector("#usernameField").onchange = function() {
+	socketData.name = this.value;
+}
+
+document.querySelector("#save").onclick = function() {
+	if (serverID) {
+		socket.emit("updateData",socketData,serverID)
+	} else {
+		socket.emit("updateData",socketData)
+	}
 }
